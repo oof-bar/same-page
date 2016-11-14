66df177390eafe0c53b00cf61f7f42038716eae0
Savannah Julian <savannah.julian@gmail.com>
2016-11-14 13:53:39 -0800

    Getting started

---
+++ app/site/templates/home.php
@@ -0,0 +1,14 @@
+<? snippet('header') ?>
+
+<section class="studio">
+  <div class="wrap">
+    <div class="side-bar">
+      <?= $page->studio_info()->kirbytext() ?>
+    </div>
+    <div class="main">
+      Photo
+    </div>
+  </div>
+</section>
+
+<? snippet('footer') ?>
