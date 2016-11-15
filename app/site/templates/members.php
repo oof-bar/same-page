40a3b6ea57d1551b2ec47d99663afd60f25006a3
Savannah Julian <savannah.julian@gmail.com>
2016-11-15 13:27:01 -0800

    Styling members page

--- app/site/templates/members.php
+++ app/site/templates/members.php
@@ -3,6 +3,26 @@
 <section class="members">
   <div class="wrap">
     <?= $page->members_info()->kirbytext() ?>
+    <ul class="member-list">
+      <? foreach ( $page->members()->toStructure() as $member ) { ?>
+        <li class="member">
+          <div class="profile-photo">
+            <?= $member->photo() ?>
+          </div>
+          <div class="about">
+            <div class="name">
+              <h3><?= $member->name() ?></h3>
+            </div>
+            <div class="bio">
+              <?= $member->bio() ?>
+            </div>
+            <div class="link">
+              <a href="<?= $member->link() ?>" class="button" target="_blank">Portfolio</a>
+            </div>
+          </div>
+        </li>
+      <? } ?>
+    </ul>
   </div>
 </section>
 
