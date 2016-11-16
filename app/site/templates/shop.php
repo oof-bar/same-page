99f988eb308a2963887d3a07a1a203e2642fee28
Savannah Julian <savannah.julian@gmail.com>
2016-11-16 14:38:53 -0800

    Product link styles

--- app/site/templates/shop.php
+++ app/site/templates/shop.php
@@ -2,23 +2,25 @@
 
 <section class="shop">
   <div class="wrap">
-    <div class="products">
+    <ul class="products">
       <? foreach ( $products as $product ) { ?>
-        <div class="product">
-          <? if ( $product_photo = $product->product_photo()->toFile() ) { ?>
-            <div class="product-photo">
-              <?= html::img( Help::resize($product_photo, 'large')->url()) ?>
+        <li class="product">
+          <a href="<?= $product->url() ?>">
+            <? if ( $product_photo = $product->product_photo()->toFile() ) { ?>
+              <div class="product-photo">
+                <?= html::img( Help::resize($product_photo, 'large')->url()) ?>
+              </div>
+            <? } ?>
+            <div class="product-name">
+              <h3><?= $product->title() ?></h3>
             </div>
-          <? } ?>
-          <div class="product-name">
-            <h3><a href="<?= $product->url() ?>"><?= $product->title() ?></a></h3>
-          </div>
-          <div class="product-price price">
-            <?= $product->price() ?>
-          </div>
-        </div>
+            <div class="product-price price">
+              <?= $product->price() ?>
+            </div>
+          </a>
+        </li>
       <? } ?>
-    </div>
+    </ul>
   </div>
 </section>
 
