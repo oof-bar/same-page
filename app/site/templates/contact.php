34953ebe016bf7c14a30bb086d879a2cdaf085f6
Savannah Julian <savannah.julian@gmail.com>
2016-11-16 12:11:31 -0800

    Contact and homepage images

---
+++ app/site/templates/contact.php
@@ -0,0 +1,7 @@
+<? snippet('header') ?>
+
+<section class="contact cover" style="background-image: url('<?= Help::resize($page->contact_photo()->toFile(), 'large')->url() ?>')">
+
+</section>
+
+<? snippet('footer') ?>
--- app/site/templates/home.php
+++ app/site/templates/home.php
@@ -1,9 +1,7 @@
 <? snippet('header') ?>
 
-<section class="studio">
-  <div class="wrap">
-    <?= $page->studio_info()->kirbytext() ?>
-  </div>
+<section class="studio cover" style="background-image: url('<?= Help::resize($page->studio_photo()->toFile(), 'large')->url() ?>')">
+
 </section>
 
 <? snippet('footer') ?>
