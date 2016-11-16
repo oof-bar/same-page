3daed7bfa891595d84c5e145feab81d71dec8ae9
Savannah Julian <savannah.julian@gmail.com>
2016-11-15 18:02:50 -0800

    Adding social links

--- app/site/snippets/footer.php
+++ app/site/snippets/footer.php
@@ -1,6 +1,18 @@
     </main>
     <footer>
-      Footer
+      <div class="legal">
+        ©<?= date('Y') ?>
+      </div>
+      <ul class="social">
+        <? foreach ( $site->social()->toStructure() as $link ) { ?>
+          <li>
+            <?= html::a($link->url(), $link->service(), [
+              'target' => '_blank',
+              'class' => 'link'
+            ]) ?>
+          </li>
+        <? } ?>
+      </ul>
     </footer>
     <?= js(['@auto', 'assets/js/app.js']) ?>
   </body>
