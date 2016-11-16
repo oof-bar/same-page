faf9f335293e827d52d47e3593a3d04b87df4d8d
Savannah Julian <savannah.julian@gmail.com>
2016-11-16 13:54:42 -0800

    Adding a general page description

--- app/site/snippets/sidebar.php
+++ app/site/snippets/sidebar.php
@@ -1,8 +1,14 @@
+<? if ( $page->depth() === 1 ) { ?>
+  <? $description = $page->description()->kirbytext() ?>
+<? } else { ?>
+  <? $description = $page->parents()->first()->description()->kirbytext() ?>
+<? } ?>
+
 <section class="sidebar">
   <div class="wrap">
     <div class="preview">
       <div class="content">
-        main content
+        <?= $description ?>
       </div>
     </div>
   </div>
