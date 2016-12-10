2951b0f2c100fa17f180479a579af7c8c72b411d
August Miller <hello@gusmiller.com>
2016-12-09 17:13:42 -0800

    Resolve sidebar function
    Always shows the top-level title and description

--- app/site/snippets/sidebar.php
+++ app/site/snippets/sidebar.php
@@ -1,14 +1,10 @@
-<? if ( $page->depth() === 1 ) { ?>
-  <? $description = $page->description()->kirbytext() ?>
-<? } else { ?>
-  <? $description = $page->parents()->first()->description()->kirbytext() ?>
-<? } ?>
+<? $target = $page->depth() === 1 ? $page : $page->parents()->first() ?>
 
 <article class="sidebar">
   <div class="preview">
-    <h2><?= $page->title() ?></h2>
+    <h2><?= $target->title()->html() ?></h2>
     <div class="content">
-      <?= $description->kirbytext() ?>
+      <?= $target->description()->kirbytext() ?>
     </div>
   </div>
 </article>
